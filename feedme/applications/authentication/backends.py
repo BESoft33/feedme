a1890a2120b2dff3def1b820c33c532c811fd481
unknown <superstargajanikant@gmail.com>
2023-07-30 19:47:05 +0545

    added authentication for user service

--- feedme/applications/authentication/backends.py
+++ feedme/applications/authentication/backends.py
@@ -2,64 +2,26 @@ from django.contrib.auth.backends import BaseBackend
 from django.contrib.auth import get_user_model
 from django.conf import settings
 from django.db.models import Q
+from applications.user_handler.models import User as UserModel
 
 
-UserModel = get_user_model()
+# UserModel = get_user_model()
 
 class EmailOrPhoneAuthentication(BaseBackend):
-    def authenticate(self, username=None, password=None, phone=None, **kwargs):
-        valid = False
+    def authenticate(self, request, username=None, password=None, **kwargs):        
         if username is None:
-            username = kwargs.get(UserModel.USERNAME_FIELD)
-        
-        if username is None and phone is None:
-            raise ValidationError("Either email or phone must be provided.")
-
-        try:
-            from django.conf import settings
-            auth_settings = settings.AUTHENTICATION_FIELDS
-        except AttributeError:
-            return None
-
-        auth_fields = auth_settings.get('auth_fields', None)
-
-
-        q_object = None
-        first_iteration = True
-        for field in auth_fields:
-            field_ = {f'{field}__iexact': username}
-
-            if first_iteration:
-                q_object = Q(**field_)
-                first_iteration = False
-            else:
-                q_object |= Q(**field_)
-
-        try:
-            user = UserModel._default_manager.get(q_object)
-
-            if valid:
-                return user
             return None
         
+        user = UserModel.customer.get(Q(email=username) | Q(phone=username))
+
+        if user and user.check_password(password):
+            return user
+        
+        return None
+
+    def get_user(self, id):
+        try:
+            return UserModel.customer.get(pk= id)
         except UserModel.DoesNotExist:
-            # Run the default password hasher once to reduce the timing
-            # difference between an existing and a nonexistent user (#20760).
-            UserModel().set_password(password)
-        else:
-            if user.check_password(password) and self.user_can_authenticate(user):
-                return user
-
-    def get_user(self, id):
-        try:
-            return User.objects.get(pk= id)
-        except User.DoesNotExist:
-            return None
-
-
-    def get_user(self, id):
-        try:
-            return User.objects.get(pk= id)
-        except User.DoesNotExist:
             return None
         
--- feedme/applications/authentication/tests.py
+++ feedme/applications/authentication/tests.py
@@ -2,6 +2,9 @@ from django.test import TestCase
 from .backends import EmailOrPhoneAuthentication
 from django.contrib.auth import authenticate
 from applications.user_handler.models import User
+from django.conf import settings
+
+UserModel = settings.AUTH_USER_MODEL
 
 
 class EmailOrPhoneAuthenticationTest(TestCase):
@@ -10,14 +13,14 @@ class EmailOrPhoneAuthenticationTest(TestCase):
         # Create a test user with the specified email
         self.email = 'bhattarais009@gmail.com'
         self.password = 'testpassword'
-        self.email_user = User.objects.create_user(
+        self.email_user = User.customers.create_user(
             email=self.email,
             password=self.password
         )
 
         # Create a test user with the specified email
         self.phone = '9803788148'
-        self.phone_user = User.objects.create_user(
+        self.phone_user = User.customers.create_user(
             phone=self.phone,
             password=self.password
         )
@@ -33,63 +36,63 @@ class EmailOrPhoneAuthenticationTest(TestCase):
     #=========================================================================
     # User exists test
     def test_user_exists(self, email='bhattarais009@gmail.com'):
-        user = User.objects.get(email = email)
+        user = User.customers.get(email = email)
         self.assertIsInstance(user, User)
 
     def test_phone_user_exists(self, phone='9803788148'):
-        user = User.objects.get(phone=phone)
+        user = User.customers.get(phone=phone)
         self.assertIsInstance(user, User)
     
     #=========================================================================
     # Email Authentication credentials test
 
     def test_password_correct_email_incorrect(self):
-        user = authenticate(email='bhattarais009@gmail.com', password= 'testpassword')
-        self.assertIsNone(user)
-
-    def test_password_correct_email_incorrect(self):
-        user = authenticate(email='bhattarais@gmail.com', password= 'testpassword')
-        self.assertIsNone(user)
+        user = authenticate(username='bhattarais@gmail.com', password= 'testpassword')
+        self.assertIsInstance(user, User)
     
     def test_password_incorrect_email_incorrect(self):
-        user = authenticate(email='bhattarais@gmail.com', password='incorrect-password')
+        user = authenticate(username='bhattarais@gmail.com', password='incorrect-password')
+        self.assertIsInstance(user, User)
+
+    def test_password_incorrect_email_correct(self):
+        user = authenticate(username='bhattarais009@gmail.com', password='incorrect-password')
         self.assertIsNone(user)
 
     def test_password_correct_email_correct(self):
-        user = authenticate(email='bhattarais009@gmail.com', password='testpassword')
+        user = authenticate(username='bhattarais009@gmail.com', password='testpassword')
         self.assertIsInstance(user, User)
 
     def test_password_email_not_provided(self):
-        user = authenticate(email=None, password= 'incorrect_password')
+        user = authenticate(username=None, password= 'incorrect_password')
         self.assertIsNone(user)
 
     def test_password_not_provided(self):
-        user = authenticate(email='bhattarais009@gmail.com', password=None)
+        user = authenticate(username='bhattarais009@gmail.com', password=None)
         self.assertIsNone(user)
 
     # ============================================================================
     # Phone Authentication credentials test
 
     def test_phone_correct_password_incorrect(self):
-        user = authenticate(phone='9803788148', password='incorrect-password')
-        self.assertIsNone(user)
+        user = authenticate(username='9803788148', password='incorrect-password')
+        self.assertFalse(user, User)
 
     def test_phone_correct_password_correct(self):
-        user = authenticate(phone='9803788148', password='testpassword')
+        user = authenticate(username='9803788148', password='testpassword')
         self.assertIsInstance(user, User)
 
     def test_phone_incorrect_password_correct(self):
-        user = authenticate(phone='9866045808', password='testpassword')
+        user = authenticate(username='9866045808', password='testpassword')
         self.assertIsNone(user)
 
     def test_phone_incorrect_password_incorrect(self):
-        user = authenticate(phone='9866045808', password='incorrect-password')
+        user = authenticate(username='9866045808', password='incorrect-password')
         self.assertIsNone(user)
     
     # ================================================================================
     # Test User Role
     def test_user_is_staff(self):
-        user = User.objects.get(phone='9803788148')
+        user = User.customers.get(phone='9803788148')
         is_staff = user.is_staff
         self.assertFalse(is_staff)
 
