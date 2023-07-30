4395b53dd89b444f3f902b1fc18b25c771d80c5b
unknown <superstargajanikant@gmail.com>
2023-07-30 19:47:39 +0545

    base application

--- feedme/applications/base/views.py
+++ feedme/applications/base/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate
+from django.http import HttpResponse
+from applications.user_handler.models import User
 
-# Create your views here.
+def index(request):
+    auth = authenticate(username='9803788148', password='@Yamaraj33')
+    return HttpResponse(f"<p>Authentication is: {auth} </p>")
