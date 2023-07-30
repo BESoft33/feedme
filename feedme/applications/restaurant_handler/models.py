e4e0ce30b103b7521074877b8e076bd087ce83f8
unknown <superstargajanikant@gmail.com>
2023-07-30 19:49:44 +0545

    added models for restaurant

--- feedme/applications/restaurant_handler/models.py
+++ feedme/applications/restaurant_handler/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.db.models import Q
+from uuid import uuid4
+from django.conf import settings
 
-# Create your models here.
+UserModel = settings.AUTH_USER_MODEL
+
+class Restaurant(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4(), editable=False)
+    name = models.CharField(max_length=200)    
+    manager = models.ForeignKey(UserModel, 
+                                on_delete=models.CASCADE, 
+                                related_name='restaurant', 
+                                related_query_name='manager',limit_choices_to=Q(role = 1))
+    
+
+class RestaurantDetails(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4(), editable=False)
+    restaruant = models.ForeignKey(Restaurant, on_delete=models.CASCADE, related_name='details', related_query_name='restaurant')
+    location = models.CharField(max_length=200)
+    open_at = models.TimeField()
+    close_at = models.TimeField()
+
+
+    # @property
+    # def is_open(self):
+    #     from datetime import datetime
+    #     now  = datetime.datetime.now().time()
+    #     return self.open_at < now < self.close_at
+
+
+
+
+
